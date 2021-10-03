7e5766e54df6ad65184094f54297564d5f3fa4a3
sannimdev <inegg.apps@gmail.com>
2021-10-03 22:23:58 +0900

    게시물 생성하기 (Controller 부분), 게시물 생성을 위한 DTO 만들기

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,6 +1,8 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { title } from 'process';
 import { Board } from './board.model';
 import { BoardsService } from './boards.service';
+import { CreateBoardDto } from './dto/create-board.dto';
 
 @Controller('boards')
 export class BoardsController {
@@ -10,5 +12,11 @@ export class BoardsController {
     getAllBoard(): Board[] {
         // 타입을 적극적으로 정의해서 다른 타입을 반환할 시 오류를 발생시켜 개발자가 알아차리게 해야 한다.
         return this.boardsService.getAllBoards();
+    }
+
+    @Post('/')
+    // createBoard(@Body('title') title: string, @Body('description') description: string) {}
+    createBoard(@Body() createBoardDto: CreateBoardDto): Board {
+        return this.boardsService.createBoard(createBoardDto);
     }
 }
---
+++ src/boards/dto/create-board.dto.ts
@@ -0,0 +1,5 @@
+// 인터페이스와 다르게 런타임에서 작동하므로 파이프와 같은 기능을 이용할 떄 유용하다.
+export class CreateBoardDto {
+    title: string;
+    description: string;
+}
