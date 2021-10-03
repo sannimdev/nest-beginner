c809ec55f53e5bd7bf36ca2b97487b06dcddd4c3
sannimdev <inegg.apps@gmail.com>
2021-10-03 21:05:12 +0900

    NestJs의 진입점과 흐름
    app.module.ts -> Controller -> Service

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+// 진입점
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [],
+    controllers: [AppController],
+    providers: [AppService],
 })
 export class AppModule {}
