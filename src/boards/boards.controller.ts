60a5dd9011becd16b2c6e517dc726c3ab72925da
sannimdev <inegg.apps@gmail.com>
2021-10-03 22:46:25 +0900

    ID로 특정 게시물 지우기

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
 import { title } from 'process';
 import { Board } from './board.model';
 import { BoardsService } from './boards.service';
@@ -24,5 +24,10 @@ export class BoardsController {
     @Get('/:id')
     getBoardById(@Param('id') id: string): Board {
         return this.boardsService.getBoardById(id);
+    }
+
+    @Delete('/:id')
+    deleteBoardById(@Param('id') id: string): void {
+        this.boardsService.deleteBoard(id);
     }
 }
