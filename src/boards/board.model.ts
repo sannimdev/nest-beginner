54cd66842fcf23fec5365967073b7881c401b648
sannimdev <inegg.apps@gmail.com>
2021-10-04 12:00:08 +0900

    데이터베이스 연동을 위해 CRUD 구현을 위해 소스 코드 수정하기 (초기화)

--- src/boards/board.model.ts
+++ src/boards/board.model.ts
@@ -1,11 +1,12 @@
-export interface Board {
-    id: string;
-    title: string;
-    description: string;
-    status: BoardStatus;
-}
+// Entity가 있으면 별도로 모델이 필요없다.
+// export interface Board {
+//     id: string;
+//     title: string;
+//     description: string;
+//     status: BoardStatus;
+// }
 
-export enum BoardStatus {
-    PUBLIC = 'PUBLIC',
-    PRIVATE = 'PRIVATE',
-}
+// export enum BoardStatus {
+//     PUBLIC = 'PUBLIC',
+//     PRIVATE = 'PRIVATE',
+// }
--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,57 +1,47 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { v1 as uuid } from 'uuid';
-import { Board, BoardStatus } from './board.model';
 import { CreateBoardDto } from './dto/create-board.dto';
 
 @Injectable()
 export class BoardsService {
-    // DB 대용 더미데이터
-    private boards: Board[] = [
-        {
-            id: '22167970-244e-11ec-bd0c-2116b9cd367c',
-            title: '안녕하세요',
-            description: 'ㅎㅎ',
-            status: BoardStatus.PUBLIC,
-        },
-    ];
-
-    getAllBoards(): Board[] {
-        return this.boards;
-    }
-
-    createBoard(createBoardDto: CreateBoardDto): Board {
-        const { title, description } = createBoardDto;
-
-        const board: Board = {
-            id: uuid(),
-            title,
-            description,
-            status: BoardStatus.PUBLIC,
-        };
-
-        this.boards.push(board);
-        return board;
-    }
-
-    getBoardById(id: string): Board {
-        // 서비스에서 예외를 발생시킨다.
-        const found = this.boards.find((board) => board.id === id);
-
-        if (!found) {
-            throw new NotFoundException(`Can't find board with id ${id}`);
-        }
-
-        return found;
-    }
-
-    deleteBoard(id: string): void {
-        const found = this.getBoardById(id);
-        this.boards = this.boards.filter((board) => board.id !== found.id);
-    }
-
-    updateBoardStatus(id: string, status: BoardStatus): Board {
-        const board = this.getBoardById(id);
-        board.status = status;
-        return board;
-    }
+    // // DB 대용 더미데이터
+    // private boards: Board[] = [
+    //     {
+    //         id: '22167970-244e-11ec-bd0c-2116b9cd367c',
+    //         title: '안녕하세요',
+    //         description: 'ㅎㅎ',
+    //         status: BoardStatus.PUBLIC,
+    //     },
+    // ];
+    // getAllBoards(): Board[] {
+    //     return this.boards;
+    // }
+    // createBoard(createBoardDto: CreateBoardDto): Board {
+    //     const { title, description } = createBoardDto;
+    //     const board: Board = {
+    //         id: uuid(),
+    //         title,
+    //         description,
+    //         status: BoardStatus.PUBLIC,
+    //     };
+    //     this.boards.push(board);
+    //     return board;
+    // }
+    // getBoardById(id: string): Board {
+    //     // 서비스에서 예외를 발생시킨다.
+    //     const found = this.boards.find((board) => board.id === id);
+    //     if (!found) {
+    //         throw new NotFoundException(`Can't find board with id ${id}`);
+    //     }
+    //     return found;
+    // }
+    // deleteBoard(id: string): void {
+    //     const found = this.getBoardById(id);
+    //     this.boards = this.boards.filter((board) => board.id !== found.id);
+    // }
+    // updateBoardStatus(id: string, status: BoardStatus): Board {
+    //     const board = this.getBoardById(id);
+    //     board.status = status;
+    //     return board;
+    // }
 }
