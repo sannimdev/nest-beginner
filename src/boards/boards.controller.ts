5bd27eca896cef3f9b0fa284a70ba012c2b8a589
sannimdev <inegg.apps@gmail.com>
2021-10-06 19:47:26 +0900

    게시물 상태 업데이트하기

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -5,13 +5,16 @@ import {
     Get,
     Param,
     ParseIntPipe,
+    Patch,
     Post,
     UsePipes,
     ValidationPipe,
 } from '@nestjs/common';
+import { BoardStatus } from './board-status.enum';
 import { Board } from './board.entity';
 import { BoardsService } from './boards.service';
 import { CreateBoardDto } from './dto/create-board.dto';
+import { BoardStatusValidationPipe } from './pipes/board-status-validation.pipe';
 
 @Controller('boards')
 export class BoardsController {
@@ -33,33 +36,11 @@ export class BoardsController {
         return this.boardsService.deleteBoard(id);
     }
 
-    // @Get('/')
-    // getAllBoard(): Board[] {
-    //     // 타입을 적극적으로 정의해서 다른 타입을 반환할 시 오류를 발생시켜 개발자가 알아차리게 해야 한다.
-    //     return this.boardsService.getAllBoards();
-    // }
-    // @Post('/')
-    // @UsePipes(ValidationPipe)
-    // // createBoard(@Body('title') title: string, @Body('description') description: string) {}
-    // createBoard(@Body() createBoardDto: CreateBoardDto): Board {
-    //     return this.boardsService.createBoard(createBoardDto);
-    // }
-    // // loaclhost:3333?id=abcde => @Param('id)
-    // @Get('/:id')
-    // getBoardById(@Param('id') id: string): Board {
-    //     return this.boardsService.getBoardById(id);
-    // }
-    // @Delete('/:id')
-    // deleteBoardById(@Param('id') id: string): void {
-    //     this.boardsService.deleteBoard(id);
-    // }
-    // @Put('/:id')
-    // updateBoardById(
-    //     @Param('id') id: string,
-    //     @Body('status', BoardStatusValidationPipe) status: BoardStatus
-    // ) {
-    //     const board = this.boardsService.getBoardById(id);
-    //     board.status = status;
-    //     return board;
-    // }
+    @Patch('/:id/status')
+    updateBoardStatus(
+        @Param('id', ParseIntPipe) id: number,
+        @Body('status', BoardStatusValidationPipe) status: BoardStatus
+    ) {
+        return this.boardsService.updateBoardStatus(id, status);
+    }
 }
--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,5 +1,6 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { BoardStatus } from './board-status.enum';
 import { Board } from './board.entity';
 import { BoardRepository } from './board.repository';
 import { CreateBoardDto } from './dto/create-board.dto';
@@ -24,44 +25,12 @@ export class BoardsService {
         }
     }
 
-    // // DB 대용 더미데이터
-    // private boards: Board[] = [
-    //     {
-    //         id: '22167970-244e-11ec-bd0c-2116b9cd367c',
-    //         title: '안녕하세요',
-    //         description: 'ㅎㅎ',
-    //         status: BoardStatus.PUBLIC,
-    //     },
-    // ];
-    // getAllBoards(): Board[] {
-    //     return this.boards;
-    // }
-    // createBoard(createBoardDto: CreateBoardDto): Board {
-    //     const { title, description } = createBoardDto;
-    //     const board: Board = {
-    //         id: uuid(),
-    //         title,
-    //         description,
-    //         status: BoardStatus.PUBLIC,
-    //     };
-    //     this.boards.push(board);
-    //     return board;
-    // }
-    // getBoardById(id: string): Board {
-    //     // 서비스에서 예외를 발생시킨다.
-    //     const found = this.boards.find((board) => board.id === id);
-    //     if (!found) {
-    //         throw new NotFoundException(`Can't find board with id ${id}`);
-    //     }
-    //     return found;
-    // }
-    // deleteBoard(id: string): void {
-    //     const found = this.getBoardById(id);
-    //     this.boards = this.boards.filter((board) => board.id !== found.id);
-    // }
-    // updateBoardStatus(id: string, status: BoardStatus): Board {
-    //     const board = this.getBoardById(id);
-    //     board.status = status;
-    //     return board;
-    // }
+    async updateBoardStatus(id: number, status: BoardStatus): Promise<Board> {
+        const board = await this.getBoardById(id);
+
+        board.status = status;
+        await this.boardRepository.save(board);
+
+        return board;
+    }
 }
