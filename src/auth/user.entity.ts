c8ef98a5e06a761fe9f6e310e71207c10e2aab41
sannimdev <inegg.apps@gmail.com>
2021-10-06 20:15:54 +0900

    회원가입 기능 구현

--- src/auth/user.entity.ts
+++ src/auth/user.entity.ts
@@ -1,8 +1,8 @@
-import { BaseEntity, Column, Entity, PrimaryColumn } from 'typeorm';
+import { BaseEntity, Column, Entity, PrimaryColumn, PrimaryGeneratedColumn } from 'typeorm';
 
-@Entity
+@Entity()
 export class User extends BaseEntity {
-    @PrimaryColumn()
+    @PrimaryGeneratedColumn()
     id: number;
 
     @Column()
