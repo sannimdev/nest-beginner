b9f0c04d9bf2c85e41b19bae3bd4b4bb4d874c35
sannimdev <inegg.apps@gmail.com>
2021-10-13 19:08:06 +0900

    커스텀 데코레이터 생성하기

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,6 +2,8 @@ import { Body, Controller, Post, Req, UseGuards, ValidationPipe } from '@nestjs/
 import { AuthGuard } from '@nestjs/passport';
 import { AuthService } from './auth.service';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
+import { GetUser } from './get-user.decorator';
+import { User } from './user.entity';
 
 // 인증 헷갈력서 강의를 다시 찍었을 정도라고 하니 회독할 때 유의할 것!
 @Controller('auth')
@@ -21,9 +23,11 @@ export class AuthController {
     }
 
     @Post('/test')
-    @UseGuards(AuthGuard()) // AuthGuard: @nestjs/passport에서 가져온 AUthGuard()를 이용하면 요청 안에 유저 정보를 넣어줄 수 있음
-    test(@Req() req) {
-        console.log('req', req);
+    @UseGuards(AuthGuard()) // AuthGuard: @nestjs/passport에서 가져온 AuthGuard()를 이용하면 요청 안에 유저 정보를 넣어줄 수 있음
+    // 커스텀 데코레이터인 @GetUser 사용하기
+    test(@GetUser() user) {
+        // console.log('req', req);
+        console.log('user', user);
         return {
             say: 'hello',
         };
