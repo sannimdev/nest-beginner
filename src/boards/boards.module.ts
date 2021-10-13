dda40013dcd2923d484a87a191e09b1dd92b4415
sannimdev <inegg.apps@gmail.com>
2021-10-13 19:15:45 +0900

    인증된 유저까지만 게시물을 보고 쓸 수 있게 해주기

--- src/boards/boards.module.ts
+++ src/boards/boards.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from 'src/auth/auth.module';
 import { BoardRepository } from './board.repository';
 import { BoardsController } from './boards.controller';
 import { BoardsService } from './boards.service';
 
 @Module({
     // 리포지터리 등록
-    imports: [TypeOrmModule.forFeature([BoardRepository])],
+    imports: [TypeOrmModule.forFeature([BoardRepository]), AuthModule],
     controllers: [BoardsController],
     providers: [BoardsService],
 })
